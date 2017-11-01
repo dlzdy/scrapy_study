c7c4afb2ef4d069284977cb97cf1620a71ca640c
dlzdy <dlzdy@163.com>
2017-11-01 23:10:11 +0800

    dd

--- scrapy_study/spiders/github.py
+++ scrapy_study/spiders/github.py
@@ -9,14 +9,5 @@ class GithubSpider(scrapy.Spider):
     allowed_domains = ['github.com']
     start_urls = ['http://github.com/']
 
-    # 重写了爬虫类的方法, 实现了自定义请求, 运行成功后会调用callback回调函数
-    def start_requests(self):
-        return [Request("https://github.com/login",
-                        meta={'cookiejar': 1}, callback=self.post_login)]
 
-    # FormRequeset
-    def post_login(self, response):
-        # 先去拿隐藏的表单参数authenticity_token
-        authenticity_token = response.xpath(
-            '//input[@name="authenticity_token"]/@value').extract_first()
-        logging.info('authenticity_token=' + authenticity_token)
+
